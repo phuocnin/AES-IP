75d555327f0c31aa657965b264368fa288b9e9d1
phuocnin <phuocnin26@gmail.com>
2025-02-26 15:48:27 +0700

    a

--- env/aes_sequence.sv
+++ env/aes_sequence.sv
@@ -14,7 +14,7 @@ class aes_sequence extends aes_base_sequence;
         task body();
             `uvm_info("aes_sequence", "Starting aes_sequence", UVM_LOW)
             repeat(1) begin
-                `uvm_do(aes_trans)
+                `uvm_do(aes_transaction)
             end
         endtask
 endclass : aes_sequence
