4582079aaf3d790b6111eef038d6f3121cd581e6
phuocnin <phuocnin26@gmail.com>
2025-02-26 15:50:50 +0700

    A

--- env/aes_sequence.sv
+++ env/aes_sequence.sv
@@ -8,13 +8,15 @@ virtual class aes_base_sequence extends uvm_sequence #(aes_transaction);
 
 class aes_sequence extends aes_base_sequence;
         `uvm_object_utils(aes_sequence)
+        aes_transaction req;
+        req = aes_transaction::type_id::create("req");
         function new(string name = "aes_sequence");
             super.new(name);
         endfunction
         task body();
             `uvm_info("aes_sequence", "Starting aes_sequence", UVM_LOW)
             repeat(1) begin
-                `uvm_do(aes_transaction)
+                `uvm_do(req);
             end
         endtask
 endclass : aes_sequence
