07f1723f25b16272ce63477b6d580ca62d231af5
phuocnin <phuocnin26@gmail.com>
2025-02-26 13:44:19 +0700

    fix trans

--- env/aes_transaction.sv
+++ env/aes_transaction.sv
@@ -1,13 +1,13 @@
 
 class aes_transaction extends uvm_sequence_item;
-  `uvm_object_utils(name)
+  `uvm_object_utils(aes_transaction)
     
     rand bit [127:0] data_input;
     rand bit [127:0] key;
     logic [127:0] data_output;
 
-    function new(string name = "aes_transaction");
-      super.new(name);
+    function new(string aes_transaction) = "aes_transaction");
+      super.new(aes_transaction);
     endfunction
     
 endclass : aes_transaction
