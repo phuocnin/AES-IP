65e0d5ad3edef7f3764764636b62b5618d2fd594
phuocnin <phuocnin26@gmail.com>
2025-02-28 10:53:40 +0700

    A

--- env/aes_monitor.sv
+++ env/aes_monitor.sv
@@ -49,6 +49,7 @@ class aes_monitor extends uvm_monitor;
             if(this.count == 0) begin
                 `uvm_info("AES_MON", "Collecting data", UVM_LOW);
                 trans = aes_transaction::type_id::create("trans");
+                `uvm_info("AES_MON", $sformatf("Received transaction: in[%h], key[%h]",vif.data_input, vif.key), UVM_LOW);
                 trans.data_input = vif.data_input;
                 trans.key = vif.key;
             end
