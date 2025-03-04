708cb07aaa964d9be0a42df0a71654f67aef3653
phuocnin <phuocnin26@gmail.com>
2025-03-04 13:56:03 +0700

    a

--- env/aes_driver.sv
+++ env/aes_driver.sv
@@ -28,9 +28,10 @@ class aes_driver extends uvm_driver #(aes_transaction);
             if(vif.rst_n == 1) begin
                 `uvm_info("aes_driver", $sformatf("reset value: %h",vif.rst_n), UVM_LOW);
                 repeat(10) begin
-                @(posedge vif.clk);
+                
                 vif.data_input <= aes_trans.data_input;
                 vif.key <= aes_trans.key;
+                @(posedge vif.clk);
             end
             seq_item_port.item_done();
         end
