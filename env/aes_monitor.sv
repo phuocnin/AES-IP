5c0950d18e97fa06fdf29968e51d307c0fce913f
phuocnin <phuocnin26@gmail.com>
2025-03-05 15:33:55 +0700

    A

--- env/aes_monitor.sv
+++ env/aes_monitor.sv
@@ -45,7 +45,7 @@ class aes_monitor extends uvm_monitor;
     task colect_send_data();
         @(posedge vif.rst_n);
         forever begin
-            wait(this.count ==0);  
+            wait(this.count ==1);  
             #1
             `uvm_info(get_type_name(), "Collecting data", UVM_LOW);
             trans = aes_transaction::type_id::create("trans");
