64975aff97a3eca697c20953141a904d48fae0db
phuocnin <phuocnin26@gmail.com>
2025-03-06 11:10:42 +0700

    A

--- env/aes_sequence.sv
+++ env/aes_sequence.sv
@@ -17,7 +17,7 @@ class aes_single_seq extends aes_base_sequence;
             repeat(1) begin
                 req = aes_transaction::type_id::create("req");
                 start_item(req);
-                $srandom($time);
+                $srandom(int'($time)); 
                 req.randomize();
                 finish_item(req);
             end
