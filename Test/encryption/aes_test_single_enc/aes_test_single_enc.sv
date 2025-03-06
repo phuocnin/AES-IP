e55197662bcb35a9eac6acf788e2f4cfefc6e23f
phuocnin <phuocnin26@gmail.com>
2025-03-06 10:53:29 +0700

    A

--- Test/encryption/aes_test_single_enc/aes_test_single_enc.sv
+++ Test/encryption/aes_test_single_enc/aes_test_single_enc.sv
@@ -1,5 +1,7 @@
 class aes_test_basic_cipher extends aes_base_test;
     `uvm_component_utils(aes_test_basic_cipher)
+    aes_single_seq aes_seq;
+
     function new(string name = "aes_test_basic_cipher", uvm_component parent = null);
         super.new(name, parent);
     endfunction
--- Test/test.sv
+++ Test/test.sv
@@ -5,7 +5,6 @@ class aes_base_test extends uvm_test;
     virtual aes_if vif;
     aes_env aes_env0;
     uvm_table_printer printer;
-    aes_single_seq aes_seq;
     
     function new(string name = "aes_base_test", uvm_component parent = null);
         super.new(name, parent);
