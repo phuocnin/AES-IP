91b0359627be3f9ac955713695b466a9cc967af9
phuocnin <phuocnin26@gmail.com>
2025-03-06 09:59:40 +0700

    A

--- env/aes_pkg.sv
+++ env/aes_pkg.sv
@@ -10,4 +10,5 @@ package aes_pkg;
     `include "aes_sequencer.sv"
     `include "aes_scoreboard.sv"
     `include "aes_env.sv"
+    `include "../Test/encryption/aes_test_single_enc/aes_test_single_enc.sv"
 endpackage: aes_pkg
