d93494be5e684103e8eec8462d934e4a64d0a0a7
phuocnin <phuocnin26@gmail.com>
2025-03-10 14:13:23 +0700

    A

--- env/aes_driver.sv
+++ env/aes_driver.sv
@@ -33,8 +33,10 @@ class aes_driver extends uvm_driver #(aes_transaction);
             end
             seq_item_port.item_done();
             if(seq_item_port.has_do_available()) begin
+                `uvm_info(get_type_name(), "Starting new transaction", UVM_LOW);
                 @(posedge vif.clk);
                 @(negedge vif.clk);
+                `uvm_info(get_type_name(), "New transaction started", UVM_LOW);
             end
         end
     end
