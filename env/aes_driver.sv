8172d2c5db8a61a5bbd91ab31d8bfaa29661e24c
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-13 14:43:06 +0700

    llllllllllll

--- env/aes_driver.sv
+++ env/aes_driver.sv
@@ -35,6 +35,7 @@ class aes_driver extends uvm_driver #(aes_transaction);
                 if(!seq_item_port.has_do_available()) begin
                     @(posedge vif.clk);
                     @(posedge vif.clk);
+                    @(posedge vif.clk);
                     seq_item_port.item_done();
                 end
                 else begin
