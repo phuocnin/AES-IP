5e6652f0d0e934646059d6da8eb0fc7255237e37
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-13 16:26:47 +0700

    fdfdfdffdfdfdf

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -23,8 +23,8 @@ module aes_tb;
         //run_test("aes_test_continuous_enc");
 
         //run_test("aes_test_definetion_dec");
-        run_test("aes_test_continuous_dec");
-        //run_test("aes_test_special_data_dec");
+        //run_test("aes_test_continuous_dec");
+        run_test("aes_test_special_data_dec");
         
     end
     initial 
--- env/aes_sequence.sv
+++ env/aes_sequence.sv
@@ -85,7 +85,7 @@ class aes_spec_case_en extends aes_base_sequence;
     endtask
 endclass : aes_spec_case_en
 
-// Plan Test for DECRYPTION 
+// Plan Test for DECRYPTION------------------------------------------------------------------------------------------------- 
 // Plan 6 
 class aes_single_de extends aes_base_sequence;
         `uvm_object_utils(aes_single_de)
@@ -120,3 +120,49 @@ class aes_multi_de extends aes_base_sequence;
         end
     endtask
 endclass : aes_multi_de
+
+//plan 8 
+class aes_spec_case_de extends aes_base_sequence;
+    `uvm_object_utils(aes_spec_case_de)
+    aes_transaction req;
+    
+    // Khai báo mảng dữ liệu
+    bit [127:0] data_inputs[4];
+    bit [127:0] keys[4];
+
+    function new(string name = "aes_spec_case_de");
+        super.new(name);
+        
+        // Gán giá trị cho mảng
+        data_inputs = '{
+            128'h00000000000000000000000000000000,
+            128'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+            128'hAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,
+            128'h55555555555555555555555555555555
+        };
+
+        keys = '{
+            128'h00000000000000000000000000000000,
+            128'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+            128'hAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,
+            128'h55555555555555555555555555555555
+        };
+    endfunction
+
+    task body();
+        `uvm_info("aes_spec_case_de", "Starting aes_spec_case_de", UVM_LOW)
+        
+        foreach (data_inputs[i]) begin
+            req = aes_transaction::type_id::create("req");
+            start_item(req);
+
+            // Dùng void'() để tránh lỗi cú pháp
+            void'(req.randomize() with {
+                data_input == data_inputs[i];
+                key == keys[i];
+            });
+
+            finish_item(req);
+        end
+    endtask
+endclass : aes_spec_case_de
