d1a57ead9a31ae9d8f6127ae53c04bfb7ddbdfcf
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-17 13:52:00 +0700

    qưqwqwqưqwqw

--- Test/reset/aes_test_reset_enc/aes_test_reset_enc.sv
+++ Test/reset/aes_test_reset_enc/aes_test_reset_enc.sv
@@ -1,13 +1,13 @@
 class aes_test_reset_enc extends aes_base_test;
     `uvm_component_utils(aes_test_reset_enc)
-    aes_multi_en aes_seq;
+    aes_multi_en aes_seq_re_enc;
     //aes_reset_seq aes_rst_seq;
     function new(string name = "aes_test_reset_enc", uvm_component parent = null);
         super.new(name, parent);
     endfunction
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        aes_seq = aes_multi_en::type_id::create("aes_seq");
+        aes_seq_re_enc = aes_multi_en::type_id::create("aes_seq_re_enc");
         
     endfunction
     task run_phase(uvm_phase phase);
@@ -16,7 +16,7 @@ class aes_test_reset_enc extends aes_base_test;
         aes_env0.scoreboard.disable_scoreboard = 1;
         phase.raise_objection(this);
         fork
-            aes_seq.start(aes_env0.sequencer);
+            aes_seq_re_enc.start(aes_env0.sequencer);
             begin
                 `uvm_info(get_type_name(), "Resetting DUT", UVM_LOW)
                 repeat (14) @(posedge vif.clk);
