68085b1d2210a1cc492087862adce5c9d3433209
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-17 14:20:11 +0700

    dsdsdsdsdsds

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -22,9 +22,10 @@ module aes_tb;
          // run_test("aes_test_reset_enc"); 
         // run_test("aes_test_reset_dec");  
 
-        run_test("aes_test_definetion_enc"); 
+        //run_test("aes_test_definetion_enc");
+        run_test("aes_test_continuous_enc"); 
         //run_test("aes_test_special_data_enc");
-        //run_test("aes_test_continuous_enc");
+        
 
         //run_test("aes_test_definetion_dec");
         //run_test("aes_test_continuous_dec");
