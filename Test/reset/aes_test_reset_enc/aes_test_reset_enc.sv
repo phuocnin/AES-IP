bb8e9e43a0fa32c982597f232a2a40c3f792c21f
phuocnin <phuocnin26@gmail.com>
2025-03-17 09:55:10 +0700

    s

--- Test/reset/aes_test_reset_enc/aes_test_reset_enc.sv
+++ Test/reset/aes_test_reset_enc/aes_test_reset_enc.sv
@@ -1,17 +1,14 @@
 class aes_test_reset_enc extends aes_base_test;
     `uvm_component_utils(aes_test_reset_enc)
-    virtual dut_if vif; 
-    aes_multi_seq aes_seq;
+    aes_multi_en aes_seq;
     //aes_reset_seq aes_rst_seq;
     function new(string name = "aes_test_reset_enc", uvm_component parent = null);
         super.new(name, parent);
     endfunction
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        aes_seq = aes_multi_seq::type_id::create("aes_seq");
+        aes_seq = aes_multi_en::type_id::create("aes_seq");
         aes_env0.aes_scoreboard.disable_scoreboard = 1;
-        if (!uvm_config_db#(virtual dut_if)::get(this, "", "vif", vif))
-            `uvm_fatal("NOVIF", {"Virtual interface must be set for: ", get_full_name()})
     endfunction
     task run_phase(uvm_phase phase);
         `uvm_info(get_type_name(), "Starting test", UVM_LOW)
