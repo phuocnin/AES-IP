fe9706553aada0dd148d0af46d039fa2b14b383f
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-17 14:39:50 +0700

    ưqwq

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -20,7 +20,7 @@ module aes_tb;
         uvm_config_db#(virtual aes_if)::set(null, "*", "vif", vif);
 
          // run_test("aes_test_reset_enc"); 
-        // run_test("aes_test_reset_dec");  
+        run_test("aes_test_reset_dec");  
 
         //run_test("aes_test_definetion_enc");
         //run_test("aes_test_continuous_enc"); 
@@ -28,7 +28,7 @@ module aes_tb;
         
 
         //run_test("aes_test_definetion_dec");
-        run_test("aes_test_continuous_dec");
+        //run_test("aes_test_continuous_dec");
         //run_test("aes_test_special_data_dec");
         
  
