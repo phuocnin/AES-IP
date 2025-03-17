a619b77c1dd3ce982939f21eb90cda3997074084
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-03-17 14:45:16 +0700

    sdsdsd

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -19,8 +19,8 @@ module aes_tb;
     initial begin
         uvm_config_db#(virtual aes_if)::set(null, "*", "vif", vif);
 
-         // run_test("aes_test_reset_enc"); 
-        run_test("aes_test_reset_dec");  
+          run_test("aes_test_reset_enc"); 
+        //run_test("aes_test_reset_dec");  
 
         //run_test("aes_test_definetion_enc");
         //run_test("aes_test_continuous_enc"); 
