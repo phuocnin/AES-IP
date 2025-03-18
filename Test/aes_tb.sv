1bf5ef6195a766f5427bd17bcdecb68df55ee4cf
phuocnin <phuocnin26@gmail.com>
2025-03-18 10:11:33 +0700

    a

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -23,13 +23,13 @@ module aes_tb;
         //run_test("aes_test_reset_dec");  
 
         //run_test("aes_test_definetion_enc");
-        run_test("aes_test_continuous_enc"); 
+        //run_test("aes_test_continuous_enc"); 
        // run_test("aes_test_special_data_enc");
         
 
         //run_test("aes_test_definetion_dec");
         //run_test("aes_test_continuous_dec");
-        //run_test("aes_test_special_data_dec");
+        run_test("aes_test_special_data_dec");
         
  
         
