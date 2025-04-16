cc4189458cc9225a2642f378ecaf4a8eb8e1f661
phuocnin <phuocnin26@gmail.com>
2025-04-16 13:29:55 +0700

    s

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -18,8 +18,8 @@ module aes_tb;
 
     initial begin
         uvm_config_db#(virtual aes_if)::set(null, "*", "vif", vif);
-        run_test();
-        //run_test("aes_test_reset_enc"); 
+        //run_test();
+        run_test("aes_test_reset_enc"); 
         //run_test("aes_test_reset_dec");  
 
         //run_test("aes_test_definetion_enc");
