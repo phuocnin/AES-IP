38c0a9cfe3275eec19b37faad09349bad475bd77
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-04-16 10:30:09 +0700

    asasa

--- Test/aes_tb.sv
+++ Test/aes_tb.sv
@@ -27,8 +27,8 @@ module aes_tb;
        // run_test("aes_test_special_data_enc");
         
 
-        run_test("aes_test_definetion_dec");
-        //run_test("aes_test_continuous_dec");
+        //run_test("aes_test_definetion_dec");
+        run_test("aes_test_continuous_dec");
         //run_test("aes_test_special_data_dec");
         
  
