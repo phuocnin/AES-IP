6b459a8ab0057a2e33f1820a1e59c99baa209798
NguyenVinh1609 <congvinh16092002@gmail.com>
2025-05-15 14:54:58 +0700

    sáasáa

--- AES_CORE/design.sv
+++ AES_CORE/design.sv
@@ -1,6 +1,6 @@
 // Code your design here
 `include "AES_CIPHER.v"
-`include "AES_DECIPHER.v"
+//`include "AES_DECIPHER.v"
 `include "AES_Config_Parameter.v" // Define function of IP here
 module AES_CORE(
     input wire [127:0] data_in,
